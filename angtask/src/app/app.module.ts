0a88d215446cbf64093d1ecf1fcdfa61d3ec0017
sathish96521 <51506359+sathish96521@users.noreply.github.com>
2019-06-08 15:11:45 +0530

    ang task

---
+++ angtask/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';
+import { ParentComponent } from './parent/parent.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChildComponent,
+    ParentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
